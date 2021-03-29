80915f2f8ec40e124a2fc90921ae68e551661536
Jun Tao Lei <44855944+juntaolei@users.noreply.github.com>
2021-03-29 00:30:20 -0400

    Minor update

--- lib/gui.ml
+++ lib/gui.ml
@@ -95,18 +95,16 @@ let cell_positions state event =
   let canvas_width = state |> canvas_size |> fst in
   let cell_width = state |> cell_size |> fst in
   let cell_height = state |> cell_size |> snd in
-  ( (mouse_x - (canvas_width / 2)) / cell_width,
-    (mouse_y - cell_height) / cell_height )
+  let x = (mouse_x - (canvas_width / 2)) / cell_width in
+  let y = (mouse_y - cell_height) / cell_height in
+  ( x + y |> float_of_int |> floor |> int_of_float,
+    y - x |> float_of_int |> floor |> int_of_float )
 
 let highlight state event =
   let canvas_width = state |> canvas_size |> fst in
   let canvas_height = state |> canvas_size |> snd in
   let map_length = state |> map_length in
   let positions = cell_positions state event in
-  print_endline
-    ((positions |> fst |> string_of_int)
-    ^ " "
-    ^ (positions |> snd |> string_of_int));
   fg_ctx##clearRect
     (float_of_int (-canvas_width))
     (float_of_int (-canvas_height))
@@ -118,7 +116,11 @@ let highlight state event =
     && fst positions >= 0
     && snd positions < map_length
   then
-    draw_cell fg_ctx state (fst positions) (snd positions) "lightgray";
+    print_endline
+      (string_of_int (fst positions)
+      ^ " "
+      ^ string_of_int (snd positions));
+  draw_cell fg_ctx state (fst positions) (snd positions) "black";
   Js._false
 
 let draw_gui state =
@@ -127,7 +129,7 @@ let draw_gui state =
   setup_canvas map_canvas map_ctx state;
   setup_canvas fg_canvas fg_ctx state;
   draw_map state;
-  Html.addEventListener fg_canvas Html.Event.mouseover
+  Html.addEventListener fg_canvas Html.Event.mousemove
     (Dom.handler (highlight state))
     Js._true
   |> ignore

087b5332e31079b53d8b26cae3f4f35fde373d08
Yijun Yin <yy389@cornell.edu>
2021-04-28 13:54:16 +0800

    updated tests

--- test/test_buildings.ml
+++ test/test_buildings.ml
@@ -1,13 +1,14 @@
 open OUnit2
 open Lib.Buildings
 
+(* This test suite covers the most basic commands in [buildings.ml] including 
+    functions for resources, roads, and buildings. *)
 let water = new_resource "water" 1
-
-(*let electricity_lst = [ new_resource "electricity" 8 ]*)
 let road_1 = new_road 10 1 2
-
 let planation_bld = new_building "oat plantation" 10 5 5 "oat" 6 7 0 ""
-
+let mine_bld = new_building "mine" 30 30 20 "iron" 5 4 3 "electricity"
+let nul_resource = new_resource "" 0
+let electricity_resource = new_resource "electricity" 3
 let resource_tests =
   [
     ( "resource name is \"water\"" >:: fun _ ->
@@ -33,6 +34,16 @@ let building_tests =
       assert_equal (income planation_bld) 6 );
     ( "defense of oat plantation is 7" >:: fun _ ->
       assert_equal (defense planation_bld) 7 );
+    ( "plantation has no resource dependency" >:: fun _ ->
+      assert_equal (resource_dependency planation_bld) [nul_resource] );
+
+    ( "building name of mine is \"mine\""
+    >:: fun _ ->
+      assert_equal (building_name mine_bld) "mine" );
+    ( "cost of min is 30" >:: fun _ ->
+      assert_equal (cost mine_bld) 30 );
+    ( "mine requires 3 units of electricity" >:: fun _ ->
+      assert_equal (resource_dependency mine_bld) [electricity_resource] );
   ]
 
 let road_tests =
--- test/test_state.ml
+++ test/test_state.ml
@@ -2,11 +2,15 @@ open OUnit2
 open Lib.State
 open Lib.Buildings
 
-let state = new_state "test_state.json" 1000 900 10 128 64
+(* This test suite covers the most basic commands in [state.ml] including 
+    functions for creating and updating states. *)
 
+let state_0 = new_state "test_state.json" 1000 900 10 128 64
 let state_1 = from_file "map_1.json"
 
-let expected_cell_array =
+(** [expected_cell_array_1] is the expected cell array array extracted from
+    file "map_1.json". *)
+let expected_cell_array_1 =
   let empty = Array.make_matrix 3 3 None in
   empty.(0).(0) <-
     Building (new_building "mine" 0 0 1 "iron" 0 0 5 "electricity");
@@ -15,6 +19,17 @@ let expected_cell_array =
     Building (new_building "oats_plantation" 0 0 10 "oat" 0 0 0 "");
   empty
 
+(** [expected_stockpile_1] is the expected stockpile extracted from file 
+  "map_1.json". *)
+  let expected_stockpile_1 = [
+  new_resource "oat" 0;
+  new_resource "electricity" 40;
+  new_resource "iron" 20;
+  new_resource "money" 100
+]
+
+(** [lst_of_array_array a] is the cell list list represented by the cell array
+    array [a]. *)
 let lst_of_array_array a =
   let acc = ref [] in
   for i = 0 to Array.length a - 1 do
@@ -27,14 +42,24 @@ let lst_of_array_array a =
   done;
   !acc
 
+(** [string_of_lst acc lst] is the string representation of cell list [lst]. *)
 let rec string_of_lst acc = function
   | [] -> acc
   | h :: t -> string_of_lst (acc ^ str_of_cell h ^ ",") t
 
+(** [string_of_lst acc lst] is the string representation of cell list list
+    [lst]. *)
 let rec string_of_lst_lst acc = function
   | [] -> acc
   | h :: t -> string_of_lst_lst ("[" ^ string_of_lst "" h ^ "]" ^ acc) t
 
+(** [string_of_stockpile acc sp] is the string representation of stockpile 
+    [sp]. *)
+let rec string_of_stockpile acc = function
+| [] -> acc
+| h :: t -> 
+  string_of_stockpile 
+    (acc ^ (resource_name h) ^ ":" ^ (string_of_int (resource_amount h))) t
 let canvas_size_test name st expected : test =
   name >:: fun _ ->
   assert_equal expected (canvas_size st) ~printer:(fun (a, b) ->
@@ -59,21 +84,25 @@ let cell_test name st expected : test =
       string_of_lst_lst "" (lst_of_array_array x))
 
 let stockpile_test name st expected : test =
-  name >:: fun _ -> assert_equal expected (stockpile st)
+  name >:: fun _ -> assert_equal expected (stockpile st) 
+  ~printer: (fun x -> string_of_stockpile "" x)
 
 let state_test =
   [
-    canvas_size_test "canvas size of state is 1000 by 900" state
+    canvas_size_test "canvas size of state_0 is 1000 by 900" state_0
       (1000, 900);
-    map_length_test "map length of state is 10" state 10;
-    cell_size_test "cell size of state is 128 by 64" state (128, 64);
+    map_length_test "map length of state_0 is 10" state_0 10;
+    cell_size_test "cell size of state_0 is 128 by 64" state_0 (128, 64);
     canvas_size_test "canvas size of state_1 is 300 by 300" state_1
       (300, 300);
     map_length_test "map length of state_1 is 3" state_1 3;
     cell_size_test "cell size of state_1 is 100 by 100" state_1
       (100, 100);
     population_test "population testing" state_1 20;
-    cell_test "cell test for state_1" state_1 expected_cell_array;
+    cell_test "cell test for state_1" state_1 expected_cell_array_1;
+    stockpile_test "stockpile for state_1" state_1 expected_stockpile_1;
+
+    (** updating states *)
   ]
 
 let test_suite = List.flatten [ state_test ]

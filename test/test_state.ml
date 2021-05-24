d989407fc45d96ca90007cecbaed8b86a88ae72d
Yijun Yin <yy389@cornell.edu>
2021-05-24 20:55:35 +0800

    update tests

--- test/test_state.ml
+++ test/test_state.ml
@@ -1,3 +1,5 @@
+(* This test suite covers the most basic commands in [state.ml]
+   including functions for creating and updating states. *)
 open OUnit2
 open Lib.State
 open Lib.Buildings
@@ -10,28 +12,50 @@ let read_file_as_string filename =
   close_in channel;
   content
 
-(* This test suite covers the most basic commands in [state.ml]
-   including functions for creating and updating states. *)
-
+(** [state_0] is the default state created with the [new_state] function. *)
 let state_0 = new_state 1000 900 10 128 64
 
+(** [state_1] is the state extracted from file [map1.json]. *)
 let state_1 = from_string (read_file_as_string "map_2.json")
 
+(** [expected_cell_array_0]is the expected cell array array extracted
+    from state_0. *)
+let expected_cell_array_0 = Array.make_matrix 10 10 None
+
 (** [expected_cell_array_1] is the expected cell array array extracted
-    from file "map_1.json". *)
+    from state_1 in file "map_1.json". *)
 let expected_cell_array_1 =
-  let empty = Array.make_matrix 3 3 None in
-  empty.(0).(0) <-
+  let empty = Array.make_matrix 9 9 None in
+  empty.(3).(0) <-
     Building
-      (new_building "mine" 0 0 ("iron", 1) 0 0 [ ("electricity", 5) ] 0
-         0 0 0 false);
-  empty.(0).(1) <-
-    Building (new_building "house" 0 0 ("", 0) 0 0 [] 0 0 0 0 false);
-  empty.(2).(0) <-
+      (new_building "power_plant" 200 0 ("electricity", 5) 5 3 [] 0 5 9
+          0 false);
+  empty.(4).(0) <-
+    Building (new_building "house" 50 0 ("", 0) 10 1 [] 0 0 0 0 false);
+  empty.(5).(0) <-
+    Building (new_building "house" 50 0 ("", 0) 10 1 [] 0 0 0 0 false);
+  empty.(6).(0) <-
+    Building (new_building "house" 50 0 ("", 0) 10 1 [] 0 0 0 0 false);
+  empty.(7).(0) <-
+    Building (new_building "house" 50 0 ("", 0) 10 1 [] 0 0 0 0 false);
+  empty.(7).(1) <-
     Building
-      (new_building "oats_plantation" 0 0 ("oat", 10) 0 0 [] 0 0 0 0
+      (new_building "oats_plantation" 50 0 ("food", 10) 5 1 [] 0 3 5 0
+         false);
+  empty.(8).(0) <-
+    Building
+      (new_building "oats_plantation" 50 0 ("food", 10) 5 1 [] 0 3 5 0
          false);
   empty
+
+let init_stockpile =
+  [
+    new_resource "money" 200;
+    new_resource "electricity" 0;
+    new_resource "food" 0;
+    new_resource "iron" 0;
+    new_resource "coal" 0;
+  ]
 
 (** [expected_stockpile_1] is the expected stockpile extracted from file
     "map_1.json". *)
@@ -82,7 +106,7 @@ let canvas_size_test name state expected : test =
   name >:: fun _ ->
   assert_equal expected state.canvas_size ~printer:(fun (a, b) ->
       string_of_int a ^ string_of_int b)
-(*
+
 let cell_size_test name state expected : test =
   name >:: fun _ ->
   assert_equal expected state.cell_size ~printer:(fun (a, b) ->
@@ -105,22 +129,26 @@ let stockpile_test name state expected : test =
   name >:: fun _ ->
   assert_equal expected state.stockpile ~printer:(fun x ->
       string_of_stockpile "" x)
-*)
+
 let state_test =
   [
-    (*
+    (* default new_state *)
     canvas_size_test "canvas size of state_0 is 1000 by 900" state_0
       (1000, 900);
     map_length_test "map length of state_0 is 10" state_0 10;
     cell_size_test "cell size of state_0 is 128 by 64" state_0 (128, 64);
-    canvas_size_test "canvas size of state_1 is 300 by 300" state_1
-      (300, 300);
-    map_length_test "map length of state_1 is 3" state_1 3;
-    cell_size_test "cell size of state_1 is 100 by 100" state_1
-      (100, 100);
-    population_test "population testing" state_1 20;
+    population_test "population state_0 is 0" state_0 0;
+    stockpile_test "stockpile of state_0 is empty" state_0 init_stockpile;
+    cell_test "cell test for state_0" state_0 expected_cell_array_0;
+
+    (* state_1 *)
+    canvas_size_test "canvas size of state_1 is 1200 by 750" state_1
+      (1200, 750);
+    map_length_test "map length of state_1 is 9" state_1 9;
+    cell_size_test "cell size of state_1 is 128 by 64" state_1
+      (128, 64);
+    population_test "population of state_1 is 16" state_1 16;
     cell_test "cell test for state_1" state_1 expected_cell_array_1;
-    stockpile_test "stockpile for state_1" state_1 expected_stockpile_1;*)
   ]
 
 let test_suite = List.flatten [ state_test ]
